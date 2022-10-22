59988af928ff95898a0341de8f2415ae37c0673d
hzy <247197021@qq.com>
2022-10-22 10:10:20 +0800

    213

---
+++ 后台项目_vue2_空/src/store/modules/user.js
@@ -0,0 +1,79 @@
+import {login as loginAPI} from '@/api'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+
+
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: '',
+    avatar: '',
+    
+    roles: [],
+    buttons: [],
+    routes: [], // 本用户所有的路由,包括了固定的路由和下面的addRouters
+    asyncRoutes: [] // 本用户的角色赋予的新增的动态路由
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  SET_USER: (state, userInfo) => {
+    state.name = userInfo.name // 用户名
+    state.avatar = userInfo.avatar // 头像
+    state.roles = userInfo.roles // 角色列表
+    state.buttons = userInfo.buttons // 按钮权限列表
+  },
+
+  SET_TOKEN (state, token) {
+    state.token = token
+  },
+
+  RESET_USER (state) {
+    Object.assign(state, getDefaultState())
+  }
+}
+
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo
+    return new Promise((resolve, reject) => {
+      loginAPI.login(username, password)
+        .then(result => {
+          const { data } = result
+          commit('SET_TOKEN', data.token)
+          setToken(data.token)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+    })
+  },
+
+  // 获取用户信息
+  async getInfo({ commit, state }) {
+    const {data} = await loginAPI.getInfo()
+    commit('SET_USER', data)
+  },
+
+  /* 
+  重置用户信息
+  */
+  async resetUser({ commit, state }) {
+    // 如果当前是登陆的, 请求退出登陆
+    if (state.name) {
+      await loginAPI.logout()
+    }
+    // 提交重置用户信息的mutation
+    commit('RESET_USER')
+  },
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
